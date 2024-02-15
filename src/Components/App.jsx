7751d20b1807fce1894f63c82fccecb70d3f5e72
koshmaniuk <trident93@gmail.com>
2024-02-15 16:25:28 -0500

    to pc

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,22 +1,69 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { ArticleList } from './ArticleList.jsx';
+import { fetchArticlesWithTopic } from '../articles-api.js';
+import { SearchForm } from './SearchForm.jsx';
+import { Loader } from './Loader.jsx';
+import { Error } from './Error.jsx';
 
 const App = () => {
-  useEffect(() => {
-    async function fetchArticles() {
-      const response = await axios.get('<https://hn.algolia.com/api/v1/search?query=react>');
-      console.log(response);
-    }
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    fetchArticles();
-  }, []);
+  const handleSearch = async topic => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
-      <h1>Latest articles</h1>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <Error />}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
 };
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         setLoading(true);
+//         const data = await fetchArticlesWithTopic('react');
+//         setArticles(data);
+//       } catch (error) {
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       {loading && <p>Loading data, please wait...</p>}
+//       {error && <p>Whoops, something went wrong! Please try reloading this page!</p>}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
 
 export default App;
